530bc35e10273a62fe60029331baf0b36c8f63fc
upendra kumar <kupendraraj144@gmail.com>
2023-07-04 14:13:44 +0530

    final commit

--- src/main/java/com/driver/controllers/HotelManagementRepository.java
+++ src/main/java/com/driver/controllers/HotelManagementRepository.java
@@ -1,4 +1,7 @@
 package com.driver.controllers;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class HotelManagementRepository {
 }
--- src/main/java/com/driver/controllers/HotelManagementService.java
+++ src/main/java/com/driver/controllers/HotelManagementService.java
@@ -1,4 +1,7 @@
 package com.driver.controllers;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HotelManagementService {
 }
